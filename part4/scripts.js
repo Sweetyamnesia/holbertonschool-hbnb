ed1b65d7d5f2d0a1dc08de697831121d13fe1ca3
Sweetyamnesia <angela.r@live.fr>
2025-07-23 14:35:36 +0200

    change the URL

--- part4/scripts.js
+++ part4/scripts.js
@@ -13,7 +13,7 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
 	async function loginUser(email, password) {
-      const response = await fetch('http://localhost:5000/api/v1/auth/login', {
+      const response = await fetch('http://localhost:5000/auth/login', {
           method: 'POST',
           headers: {
               'Content-Type': 'application/json'
