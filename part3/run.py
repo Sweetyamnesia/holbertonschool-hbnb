f33fd729832a16336428d7f108cb229fff188bd4
Sweetyamnesia <angela.r@live.fr>
2025-07-23 15:25:18 +0200

    add line for database

--- part3/run.py
+++ part3/run.py
@@ -1,6 +1,9 @@
-from app import create_app
+from app import create_app, db
+from config import DevelopmentConfig
 
-app = create_app()
+app = create_app(DevelopmentConfig)
+with app.app_context():
+	db.create_all()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+  	app.run(debug=True) 
